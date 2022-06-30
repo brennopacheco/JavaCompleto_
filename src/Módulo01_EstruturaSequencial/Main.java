4e95d78e7edbab0152081dd69b2bae2f0881232a
brennopacheco <brenno_pacheco@hotmail.com>
2022-06-30 10:37:28 -0300

    Initial commit

---
+++ src/Módulo01_EstruturaSequencial/Main.java
@@ -0,0 +1,11 @@
+package Módulo01_EstruturaSequencial;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int y = 32;
+
+        System.out.println(y);
+        System.out.println("Good morning");
+    }
+}
